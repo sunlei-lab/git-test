bd51a5c941084b54d1d443c240076b76684fea57
sunlei-lab <73767961+sunlei-lab@users.noreply.github.com>
2021-07-28 22:08:47 +0800

    pull test

--- src/main/java/com/atguigu/Git.java
+++ src/main/java/com/atguigu/Git.java
@@ -8,5 +8,6 @@ public class Git {
         System.out.println("hello git4");
         System.out.println("hot fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
